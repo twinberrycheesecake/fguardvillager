5d034e2430a20b96885754025b6328bf0a05afc8
kokalkon <eey33@icloud.com>
2022-07-08 03:09:41 +0900

    brave attempt to armor mesh
    succesful

--- src/main/java/tallestegg/guardvillagers/client/models/GuardArmorModel.java
+++ src/main/java/tallestegg/guardvillagers/client/models/GuardArmorModel.java
@@ -9,6 +9,7 @@ import net.minecraft.client.model.geom.builders.LayerDefinition;
 import net.minecraft.client.model.geom.builders.MeshDefinition;
 import net.minecraft.client.model.geom.builders.PartDefinition;
 import tallestegg.guardvillagers.entities.Guard;
+import tallestegg.guardvillagers.client.models.GuardModel;
 
 public class GuardArmorModel extends HumanoidModel<Guard> {
     public GuardArmorModel(ModelPart part) {
@@ -16,15 +17,21 @@ public class GuardArmorModel extends HumanoidModel<Guard> {
     }
 
     public static LayerDefinition createOuterArmorLayer() {
-        MeshDefinition meshdefinition = HumanoidModel.createMesh(new CubeDeformation(1.0F), 0.0F);
+        MeshDefinition meshdefinition = GuardModel.createMesh(new CubeDeformation(1.0F), 0.0F);
         PartDefinition partdefinition = meshdefinition.getRoot();
         partdefinition.addOrReplaceChild("head",
                 CubeListBuilder.create().texOffs(0, 0).addBox(-3F, -10.0F, -4.0F, 8.0F, 8.0F, 8.0F, new CubeDeformation(1.0F)),
                 PartPose.offset(0.0F, 1.0F, 0.0F));
+        partdefinition.addOrReplaceChild("body", CubeListBuilder.create().texOffs(16, 20).addBox(-5.5F, -3.0F, -3.0F, 11.0F, 7.0F, 9.0F, new CubeDeformation(1.0F))
+                .texOffs(1, 95).addBox(-6.0F, 4.0F, -5.0F, 12.0F, 10.0F, 11.0F, new CubeDeformation(1.0F)), PartPose.offset(0.0F, 0.0F, 0.0F));
+        partdefinition.addOrReplaceChild("right_leg", CubeListBuilder.create().texOffs(0, 16).mirror().addBox(-0.4F, 2.0F, -3.0F, 4.0F, 10.0F, 6.0F, new CubeDeformation(1.0F)).mirror(false), PartPose.offset(1.9F, 12.0F, 0.0F));
+        partdefinition.addOrReplaceChild("left_leg", CubeListBuilder.create().texOffs(0, 16).mirror().addBox(-3.6F, 2.0F, -3.0F, 4.0F, 10.0F, 6.0F, new CubeDeformation(1.0F)).mirror(false), PartPose.offset(-1.9F, 12.0F, 0.0F));
+        partdefinition.addOrReplaceChild("right_arm", CubeListBuilder.create().texOffs(40, 16).addBox(-3.0F, -2.5F, 0.0F, 3.0F, 11.0F, 4.0F, new CubeDeformation(1.0F)), PartPose.offset(0.0F, 0.0F, 0.0F));
+        partdefinition.addOrReplaceChild("left_arm", CubeListBuilder.create().texOffs(40, 16).mirror().addBox(0.0F, -2.5F, 0.0F, 3.0F, 11.0F, 4.0F, new CubeDeformation(1.0F)).mirror(false), PartPose.offset(0.0F, 0.0F, 0.0F));
         return LayerDefinition.create(meshdefinition, 64, 32);
     }
     public static LayerDefinition createInnerArmorLayer() {
-        MeshDefinition meshdefinition = HumanoidModel.createMesh(new CubeDeformation(0.5F), 0.0F);
+        MeshDefinition meshdefinition = GuardModel.createMesh(new CubeDeformation(0.5F), 0.0F);
         return LayerDefinition.create(meshdefinition, 64, 32);
     }
 }
