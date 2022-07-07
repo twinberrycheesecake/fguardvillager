416c813e8355a858f25d1feee27f414a71efa03e
kokalkon <eey33@icloud.com>
2022-07-07 23:15:27 +0900

    headband texture

--- src/main/java/tallestegg/guardvillagers/client/models/GuardModel.java
+++ src/main/java/tallestegg/guardvillagers/client/models/GuardModel.java
@@ -41,8 +41,8 @@ public class GuardModel extends HumanoidModel<Guard> {
                 .texOffs(0, 116).addBox(-1.5F, -6.0F, -6.0F, 5.0F, 3.0F, 3.0F, new CubeDeformation(0.0F))
                 .texOffs(26, 72).mirror().addBox(3.5F, -11.0F, -1.0F, 2.0F, 2.0F, 1.0F, new CubeDeformation(0.0F)).mirror(false)
                 .texOffs(26, 72).addBox(-3.5F, -11.0F, -1.0F, 2.0F, 2.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(-1.0F, 0.0F, 0.0F));
-        PartDefinition rightArm = partdefinition.addOrReplaceChild("right_arm", CubeListBuilder.create().texOffs(50, 94).addBox(-3.0F, -2.5F, -2.0F, 3.0F, 11.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.0F, 0.0F));
-        PartDefinition leftArm = partdefinition.addOrReplaceChild("left_arm", CubeListBuilder.create().texOffs(50, 94).mirror().addBox(0.0F, -2.5F, -2.0F, 3.0F, 11.0F, 4.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offset(0.0F, 0.0F, 0.0F));
+        PartDefinition rightArm = partdefinition.addOrReplaceChild("right_arm", CubeListBuilder.create().texOffs(50, 94).addBox(-3.0F, -2.5F, 0.0F, 3.0F, 11.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.0F, 0.0F));
+        PartDefinition leftArm = partdefinition.addOrReplaceChild("left_arm", CubeListBuilder.create().texOffs(50, 94).mirror().addBox(0.0F, -2.5F, 0.0F, 3.0F, 11.0F, 4.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offset(0.0F, 0.0F, 0.0F));
         torso.addOrReplaceChild("quiver", CubeListBuilder.create().texOffs(100, 0).addBox(-2.5F, -4.0F, 4F, 5, 10, 5,
                 new CubeDeformation(0.0F)), PartPose.offset(0.5F, 1.0F, 2.3F));
 
