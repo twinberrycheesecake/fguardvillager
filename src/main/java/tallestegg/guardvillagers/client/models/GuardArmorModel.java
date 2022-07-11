2f3fed9e98643a037027bab9c8c07ca4a41ab629
kokalkon <eey33@icloud.com>
2022-07-12 03:18:55 +0900

    LEGGINGS, smaller helm

--- src/main/java/tallestegg/guardvillagers/client/models/GuardArmorModel.java
+++ src/main/java/tallestegg/guardvillagers/client/models/GuardArmorModel.java
@@ -20,7 +20,7 @@ public class GuardArmorModel extends HumanoidModel<Guard> {
         MeshDefinition meshdefinition = GuardModel.createMesh(new CubeDeformation(1.0F), 0.0F);
         PartDefinition partdefinition = meshdefinition.getRoot();
         partdefinition.addOrReplaceChild("head",
-                CubeListBuilder.create().texOffs(0, 0).addBox(-3F, -10.0F, -4.0F, 8.0F, 8.0F, 8.0F, new CubeDeformation(1.0F)),
+                CubeListBuilder.create().texOffs(0, 0).addBox(-3F, -10.0F, -4.0F, 8.0F, 8.0F, 8.0F, new CubeDeformation(0.25F)),
                 PartPose.offset(0.0F, 1.0F, 0.0F));
         partdefinition.addOrReplaceChild("body", CubeListBuilder.create().texOffs(16, 20).addBox(-5.5F, -3.0F, -3.0F, 11.0F, 7.0F, 9.0F, new CubeDeformation(1.0F))
                 .texOffs(1, 95).addBox(-6.0F, 4.0F, -5.0F, 12.0F, 10.0F, 11.0F, new CubeDeformation(1.0F)), PartPose.offset(0.0F, 0.0F, 0.0F));
@@ -32,6 +32,10 @@ public class GuardArmorModel extends HumanoidModel<Guard> {
     }
     public static LayerDefinition createInnerArmorLayer() {
         MeshDefinition meshdefinition = GuardModel.createMesh(new CubeDeformation(0.5F), 0.0F);
+        PartDefinition partdefinition = meshdefinition.getRoot();
+        partdefinition.addOrReplaceChild("right_leg", CubeListBuilder.create().texOffs(0, 16).mirror().addBox(-0.4F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, new CubeDeformation(1.1F)).mirror(false), PartPose.offset(1.9F, 12.0F, 0.0F));
+        partdefinition.addOrReplaceChild("left_leg", CubeListBuilder.create().texOffs(0, 16).mirror().addBox(-3.6F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, new CubeDeformation(1.1F)).mirror(false), PartPose.offset(-1.9F, 12.0F, 0.0F));
+
         return LayerDefinition.create(meshdefinition, 64, 32);
     }
 }
