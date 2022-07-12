35d09ff9768225f98912fed05d516c1b39e93974
kokalkon <eey33@icloud.com>
2022-07-12 17:04:05 +0900

    need bigger shoulder

--- src/main/java/tallestegg/guardvillagers/client/models/GuardArmorModel.java
+++ src/main/java/tallestegg/guardvillagers/client/models/GuardArmorModel.java
@@ -24,7 +24,7 @@ public class GuardArmorModel extends HumanoidModel<Guard> {
                 PartPose.offset(0.0F, 1.0F, 0.0F));
         partdefinition.addOrReplaceChild("body", CubeListBuilder.create().texOffs(16, 16).addBox(-4.0F, -0.5F, -1.5F, 8.0F, 8.0F, 4.0F, new CubeDeformation(2F))
                 .texOffs(16, 16).addBox(-4.0F, -1.5F, 0.5F, 8.0F, 8.0F, 4.0F, new CubeDeformation(1.75F))
-                .texOffs(16, 16).addBox(-4.0F, 4.8F, -3.0F, 8.0F, 9.0F, 4.0F, new CubeDeformation(2.2F))
+                .texOffs(16, 16).addBox(-4.0F, 2.8F, -3.0F, 8.0F, 9.0F, 4.0F, new CubeDeformation(2.2F))
                 .texOffs(16, 16).addBox(-4.0F, 5.8F, -0.05F, 8.0F, 6.0F, 4.0F, new CubeDeformation(2.1F)), PartPose.offset(0.0F, 0.0F, 0.0F));
         partdefinition.addOrReplaceChild("right_leg", CubeListBuilder.create().texOffs(0, 16).mirror().addBox(-0.4F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, new CubeDeformation(1.5F)).mirror(false), PartPose.offset(1.9F, 12.0F, 0.0F));
         partdefinition.addOrReplaceChild("left_leg", CubeListBuilder.create().texOffs(0, 16).mirror().addBox(-3.6F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, new CubeDeformation(1.5F)).mirror(false), PartPose.offset(-1.9F, 12.0F, 0.0F));
@@ -35,8 +35,8 @@ public class GuardArmorModel extends HumanoidModel<Guard> {
     public static LayerDefinition createInnerArmorLayer() {
         MeshDefinition meshdefinition = GuardModel.createMesh(new CubeDeformation(0.5F), 0.0F);
         PartDefinition partdefinition = meshdefinition.getRoot();
-        partdefinition.addOrReplaceChild("right_leg", CubeListBuilder.create().texOffs(0, 16).mirror().addBox(-0.4F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, new CubeDeformation(1.1F)).mirror(false), PartPose.offset(1.9F, 12.0F, 0.0F));
-        partdefinition.addOrReplaceChild("left_leg", CubeListBuilder.create().texOffs(0, 16).mirror().addBox(-3.6F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, new CubeDeformation(1.1F)).mirror(false), PartPose.offset(-1.9F, 12.0F, 0.0F));
+        partdefinition.addOrReplaceChild("right_leg", CubeListBuilder.create().texOffs(0, 16).mirror().addBox(-0.4F, 0.0F, -2.1F, 4.0F, 12.0F, 4.0F, new CubeDeformation(1.1F)).mirror(false), PartPose.offset(1.9F, 12.0F, 0.0F));
+        partdefinition.addOrReplaceChild("left_leg", CubeListBuilder.create().texOffs(0, 16).mirror().addBox(-3.6F, 0.0F, -2.1F, 4.0F, 12.0F, 4.0F, new CubeDeformation(1.1F)).mirror(false), PartPose.offset(-1.9F, 12.0F, 0.0F));
 
         return LayerDefinition.create(meshdefinition, 64, 32);
     }
